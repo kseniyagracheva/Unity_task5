97564954d4f70b738c482ecb944a2f86db6190bd
Alena <alena.vershinina.1996@gmail.com>
2023-03-26 12:40:55 +0300

    n commit

--- Assets/scritps/player input.cs
+++ Assets/scritps/player input.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+
     [SerializeField] private InputActionProperty _gripAction;
     [SerializeField] private InputActionProperty _activateAction;
+
     private void Update()
     {
         var gripValue = _gripAction.action.ReadValue<float>();
