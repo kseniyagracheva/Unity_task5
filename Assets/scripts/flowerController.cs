06873a386393d6ad6db126908cbe8ade234f6938
Alena <alena.vershinina.1996@gmail.com>
2023-04-15 18:27:14 +0300

    аааа

---
+++ Assets/scripts/flowerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flowerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+
+    [SerializeField] private float _power;
+
+    private const string KNIFE_TAG = "Knife";
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (!collider.CompareTag("KNIFE_TAG"))
+        {
+            return;
+        }
+
+        _rigidbody.isKinematic = false;
+        _rigidbody.AddForce(Vector3.up * _power);
+
+    }
+}
+
