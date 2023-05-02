b1e8539259263bfac80d7a7fa50d396cab5b7e65
kseniyagracheva <128287974+kseniyagracheva@users.noreply.github.com>
2023-05-02 13:24:14 +0300

    миша

--- Assets/scripts/PlantGrowth.cs
+++ Assets/scripts/PlantGrowth.cs
@@ -4,33 +4,10 @@ using UnityEngine;
 
 public class PlantGrowth : MonoBehaviour
 {
-    private int CurrentPosition = 0;
-    public int TimeBetweenPositions;
-    public int MaxGrowht;
-
-    public void Growth()
+    [SerializeField] private GameObject GrowthPoint;
+    private void OnTriggerEnter(Collider other)
     {
-        if (CurrentPosition!=MaxGrowht)
-        {
-            gameObject.transform.GetChild(CurrentPosition).gameObject.SetActive(true);
-        }
-        if (CurrentPosition>0 && CurrentPosition<MaxGrowht)
-        {
-            gameObject.transform.GetChild(CurrentPosition-1).gameObject.SetActive(false);
-        }
-        if (CurrentPosition < MaxGrowht)
-        {
-            CurrentPosition++;
-        }
-    }
-    void Start()
-    {
-        InvokeRepeating("Growth",TimeBetweenPositions,TimeBetweenPositions);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        other.gameObject.transform.parent = transform;
+        other.gameObject.transform.localPosition = GrowthPoint.transform.localPosition;
     }
 }
